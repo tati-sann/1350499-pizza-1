dc38dfd04817b8059d9268d7484cb92579702ef4
tati_sann <tanyawow376@gmail.com>
2021-07-26 10:06:16 +0400

    Add BaseCounter, Add BaseCounterButton

---
+++ src/frontend/src/common/components/BaseCounterButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    type="button"
+    class="counter__button"
+    :class="[
+      isPlus ? 'counter__button--plus' : 'counter__button--minus',
+      { 'counter__button--disabled': isDisabled },
+    ]"
+    :disabled="isDisabled"
+    @click="$emit('click')"
+  >
+    <span class="visually-hidden">
+      {{ isPlus ? "Больше" : "Меньше" }}
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseCounterButton",
+  props: {
+    isPlus: {
+      type: Boolean,
+      default: () => false,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
