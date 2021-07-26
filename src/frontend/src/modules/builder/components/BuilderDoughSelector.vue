ae36c67d163c066fb7123cd114d921889f8072a5
tati-sann <tanyawow@gmail.com>
2021-07-26 13:45:09 +0400

    Add components BuilderDoughSelector, Add BuilderDoughSelector to Index

---
+++ src/frontend/src/modules/builder/components/BuilderDoughSelector.vue
@@ -0,0 +1,45 @@
+<template>
+  <base-sheet content-classes="dough">
+    <template #title> Выберите тесто </template>
+
+    <template #content>
+      <label
+        v-for="(type, index) in pizzaTypeDough"
+        :key="type.nameEng"
+        class="dough__input"
+        :class="`dough__input--${type.nameEng}`"
+      >
+        <input
+          type="radio"
+          name="dought"
+          :value="type.nameEng"
+          class="visually-hidden"
+          :checked="index === 0"
+        />
+        <b>
+          {{ type.name }}
+        </b>
+        <span>
+          {{ type.description }}
+        </span>
+      </label>
+    </template>
+  </base-sheet>
+</template>
+
+<script>
+import BaseSheet from "../../../common/components/BaseSheet";
+
+export default {
+  name: "BuilderDoughSelector",
+  components: {
+    BaseSheet,
+  },
+  props: {
+    pizzaTypeDough: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
--- src/frontend/src/views/Index.vue
+++ src/frontend/src/views/Index.vue
@@ -4,32 +4,7 @@
       <div class="content__wrapper">
         <base-title>Конструктор пиццы</base-title>
         <div class="content__dough">
-          <base-sheet content-classes="dough">
-            <template #title> Выберите тесто </template>
-
-            <template #content>
-              <label
-                v-for="(type, index) in pizzaTypeDough"
-                :key="type.nameEng"
-                class="dough__input"
-                :class="`dough__input--${type.nameEng}`"
-              >
-                <input
-                  type="radio"
-                  name="dought"
-                  :value="type.nameEng"
-                  class="visually-hidden"
-                  :checked="index === 0"
-                />
-                <b>
-                  {{ type.name }}
-                </b>
-                <span>
-                  {{ type.description }}
-                </span>
-              </label>
-            </template>
-          </base-sheet>
+          <builder-dough-selector :pizza-type-dough="pizzaTypeDough" />
         </div>
 
         <div class="content__diameter">
@@ -147,10 +122,12 @@ import BaseTitle from "../common/components/BaseTitle";
 import BaseSheet from "../common/components/BaseSheet";
 import BaseRadioButton from "../common/components/BaseRadioButton";
 import BaseCounter from "../common/components/BaseCounter";
+import BuilderDoughSelector from "../modules/builder/components/BuilderDoughSelector";
 
 export default {
   name: "IndexHone",
   components: {
+    BuilderDoughSelector,
     BaseCounter,
     BaseRadioButton,
     BaseSheet,
