c1307701816ab7057ada3e2221aa8a53f616afde
tati_sann <tanyawow376@gmail.com>
2021-07-26 10:06:28 +0400

    Add BaseRadioButton

---
+++ src/frontend/src/common/components/BaseRadioButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <label class="radio">
+    <input
+      type="radio"
+      :value="value"
+      :checked="isChecked"
+      @change="$emit('change', $event.target.value)"
+    />
+    <span>
+      {{ label }}
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "BaseRadioButton",
+  model: {
+    prop: "modelValue",
+    event: "change",
+  },
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      default: () => "",
+    },
+  },
+  computed: {
+    isChecked() {
+      return this.modelValue === this.value;
+    },
+  },
+};
+</script>
