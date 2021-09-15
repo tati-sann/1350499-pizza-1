a80d9407dc6ffe9238ccd4b233f59b5b80463fd3
tati-sann <tanyawow@gmail.com>
2021-09-15 17:04:27 +0400

    Add BaseTextInput

---
+++ src/frontend/src/common/components/BaseTextInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <label class="input">
+    <span :class="isShowLabel ? '' : 'visually-hidden'">{{ label }}</span>
+    <input
+      type="text"
+      :placeholder="placeholder"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "BaseTextInput",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    isShowLabel: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
