26c4d4645047890f7731045c996063b9c758b91d
tati-sann <tanyawow@gmail.com>
2021-09-16 14:26:53 +0400

    Add AppLayoutHeader, Add AppLayout

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <app-layout-header />
+
+    <main class="content">
+      <!--      <router-view />-->
+      <slot name="main" />
+    </main>
+  </div>
+</template>
+
+<script>
+// Components
+import AppLayoutHeader from "./components/AppLayoutHeader";
+
+export default {
+  name: "AppLayout",
+  components: {
+    AppLayoutHeader,
+  },
+};
+</script>
+
+<style scoped></style>
--- src/frontend/src/views/Index.vue
+++ src/frontend/src/views/Index.vue
@@ -1,55 +1,66 @@
 <template>
-  <main class="content">
-    <form action="#" method="post">
-      <div class="content__wrapper">
-        <base-title>Конструктор пиццы</base-title>
-        <div class="content__dough">
-          <builder-dough-selector
-            :pizza-type-dough="pizzaTypeDough"
-            @setDoughType="setDoughType"
-          />
-        </div>
+  <app-layout>
+    <template #main>
+      <form action="#" method="post">
+        <div class="content__wrapper">
+          <base-title>Конструктор пиццы</base-title>
+          <div class="content__dough">
+            <builder-dough-selector
+              :pizza-type-dough="pizzaTypeDough"
+              @setDoughType="setDoughType"
+            />
+          </div>
 
-        <div class="content__diameter">
-          <builder-size-selector :pizza-sizes="pizzaSizes" />
-        </div>
+          <div class="content__diameter">
+            <builder-size-selector :pizza-sizes="pizzaSizes" />
+          </div>
 
-        <div class="content__ingridients">
-          <builder-ingredients-selector
-            :pizza-sauces="pizzaSauces"
-            :pizza-ingredients="pizzaIngredients"
-            @setIngredientValue="setIngredientValue"
-          />
-        </div>
+          <div class="content__ingridients">
+            <builder-ingredients-selector
+              :pizza-sauces="pizzaSauces"
+              :pizza-ingredients="pizzaIngredients"
+              @setIngredientValue="setIngredientValue"
+            />
+          </div>
 
-        <div class="content__pizza">
-          <base-text-input
-            v-model="pizzaName"
-            placeholder="Введите название пиццы"
-            label="Название пиццы"
-          />
+          <div class="content__pizza">
+            <base-text-input
+              v-model="pizzaName"
+              placeholder="Введите название пиццы"
+              label="Название пиццы"
+            />
 
-          <div class="content__constructor">
-            <div class="pizza pizza--foundation--big-tomato">
-              <div class="pizza__wrapper">
-                <div class="pizza__filling pizza__filling--ananas"></div>
-                <div class="pizza__filling pizza__filling--bacon"></div>
-                <div class="pizza__filling pizza__filling--cheddar"></div>
+            <div class="content__constructor">
+              <div class="pizza pizza--foundation--big-tomato">
+                <div class="pizza__wrapper">
+                  <div class="pizza__filling pizza__filling--ananas"></div>
+                  <div class="pizza__filling pizza__filling--bacon"></div>
+                  <div class="pizza__filling pizza__filling--cheddar"></div>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="content__result">
-            <p>Итого: 0 ₽</p>
-            <base-button disabled> Готовьте! </base-button>
+            <div class="content__result">
+              <p>Итого: 0 ₽</p>
+              <base-button disabled> Готовьте! </base-button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
-  </main>
+      </form>
+    </template>
+  </app-layout>
 </template>
 
 <script>
+// Components
+import AppLayout from "../layouts/AppLayout";
+import BaseTitle from "../common/components/BaseTitle";
+import BuilderSizeSelector from "../modules/builder/components/BuilderSizeSelector";
+import BuilderIngredientsSelector from "../modules/builder/components/builderIngredients/BuilderIngredientsSelector";
+import BuilderDoughSelector from "../modules/builder/components/builderDough/BuilderDoughSelector";
+import BaseTextInput from "../common/components/BaseTextInput";
+import BaseButton from "../common/components/BaseButton";
+
 // Static
 import pizza from "@/static/pizza.json";
 
@@ -60,16 +71,11 @@ import {
   normalizePizzaSizes,
   normalizePizzaSauces,
 } from "../common/helpers";
-import BaseTitle from "../common/components/BaseTitle";
-import BuilderSizeSelector from "../modules/builder/components/BuilderSizeSelector";
-import BuilderIngredientsSelector from "../modules/builder/components/builderIngredients/BuilderIngredientsSelector";
-import BuilderDoughSelector from "../modules/builder/components/builderDough/BuilderDoughSelector";
-import BaseTextInput from "../common/components/BaseTextInput";
-import BaseButton from "../common/components/BaseButton";
 
 export default {
   name: "IndexHone",
   components: {
+    AppLayout,
     BaseButton,
     BaseTextInput,
     BuilderDoughSelector,
